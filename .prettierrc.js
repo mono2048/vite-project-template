edb423dac9e1a5c0f89c6d6c0fea8cb523bcb1d6
mono <mono2048@163.com>
2022-05-07 14:38:32 +0800

    忽略文件

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// 使用空格缩
+	useTabs: true,
+	// 指定每个缩进级别的空格数
+	tabWidth: 2,
+	// 一行最多多少个字符
+	printWidth: 100,
+	// 使用双引号
+	singleQuote: false,
+	// 在语句末尾打印分号
+	semi: true,
+	// 换行符使用 lf 结尾是 可选值"<auto|lf|crlf|cr>"
+	endOfLine: "crlf",
+	trailingComma: "none"
+};
